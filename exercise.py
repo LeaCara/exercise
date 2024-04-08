e02aaebec25e625d70bc157a45044b0c852c06c2
lcarancini <leandro.carancini@gmail.com>
2024-04-07 22:24:44 -0300

    new exercise

--- exercise.py
+++ exercise.py
@@ -11,26 +11,40 @@ lista_personas = [
 def ordenar(lista_personas):
     """ El metodo debe devolver una lista con las edades ordenadas de menor a mayor"""
     # Completar
-    pass
+    # pass
+    lista_ordenada = sorted(lista_personas, key=lambda x: x[3])
+    return lista_ordenada
 
 
 def convertir_a_diccionario(lista_personas):
     """ Hacer un diccionario que tenga como claves los “dni” y como valores tuplas con nombre, apellido y edad """
     # Completar
-    pass
+    # pass
+    my_dic = {persona[0]: (persona[1], persona[2], persona[3]) for persona in lista_personas}
+    return my_dic
 
 
 def devolver_edad(lista_personas, dni):
     """ Para la 'lista_personas' devuelve la edad de la persona que tenga el dni definido.
     Tip: intentar usar el método convertir_a_diccionario"""
     # Completar
-    pass
+    # pass
+    dic_lista = convertir_a_diccionario(lista_personas)
+    if dni in dic_lista:
+        return dic_lista[dni][2] 
+    else:
+        print("No existe dni en lista")
+
+
 
 
 def eliminar_repetidos(lista_personas):
     """ El metodo debe devolver los elementos unicos """
     # Completar
-    pass
+    # pass
+    set_personas = set(lista_personas)
+    lista_personas_unicas = list(set_personas)
+    return lista_personas_unicas
 
 
 def separar_por_edad(lista_personas):
@@ -39,14 +53,32 @@ def separar_por_edad(lista_personas):
     * lista 2: menores de 25
     """
     # Completar
-    return [], []
+    lista_ordenada = ordenar(lista_personas)
+    lista_menor_25 = []
+    lista_mayor_25 = []
+    for persona in lista_ordenada:
+        if persona[3] < 25:
+            lista_menor_25.append(persona)
+        else:
+            lista_mayor_25.append(persona) 
+    return lista_mayor_25, lista_menor_25
+
 
 
 def obtener_promedio(lista):
     """ Implementar obtener el promedio de la lista de números que se recibe.
     Capturar con un try/except la excepción de dividir por cero"""
     # Completar
-    pass
+    # pass
+    sum = 0
+    prom = 0
+    try:
+        for persona in lista:
+            sum += persona[3]
+        prom = sum / len(lista)
+        return prom
+    except ZeroDivisionError:
+        return "Lista Vacia"
 
 
 def main():
@@ -60,3 +92,6 @@ def main():
     print(' * Los menores de 25 son: %s\n' % separar_por_edad(lista_personas)[1])
     print(' * El promedio de las edades es: %s\n' % obtener_promedio(ordenar(lista_personas)))
     print(' * El promedio de una lista vacía es: %s\n' % obtener_promedio([]))
+
+if __name__ == "__main__":
+    main()
